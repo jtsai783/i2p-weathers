3c48fafcfca6f99cb4091c4a4d3b7fd14ae68a2d
Jim Tsai <jim@floatleft.tv>
2023-02-24 03:51:12 -0500

    changed days to periods

--- src/components/PeriodSimple.tsx
+++ src/components/PeriodSimple.tsx
@@ -5,12 +5,31 @@ import { Weathers } from '../interfaces/WeatherEnums'
 function PeriodSimple() {
   return (
       <div className="
-        border-slate-300 border-solid border-0 border-r-2 last:border-0
-        col-span-2
-        flex items-center justify-center
+        flex items-center justify-evenly
       ">
-        <WeatherIcon weather={Weathers.Day} />
-        <span className="text-slate-50 text-[5vh] font-bold">{'30\u00b0'}</span>
+          <div className="
+          text-slate-50 text-[3vh]
+          flex flex-col justify-center items-center
+          col-span-1
+          
+        ">
+          <div className="
+            text-[3vh] font-bold
+          ">5/12
+          </div>
+          <div className="
+            text-[2vh]
+          ">
+            Day
+          </div>
+        </div>
+        <div className="
+          flex flex-row items-center
+        ">
+          <WeatherIcon weather={Weathers.Day} />
+          <span className="text-slate-50 text-[5vh] font-bold">{'30\u00b0'}</span>
+        </div>
+        
       </div>
   );
 }
--- src/pages/InformationPage.tsx
+++ src/pages/InformationPage.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import dayBg from '../assets/backgroundImages/day.png'
 import nightBg from '../assets/backgroundImages/night.jpeg'
 import { getHours } from 'date-fns'
-import DaySimple from '../components/DaySimple'
+import PeriodSimple from '../components/PeriodSimple'
 import { Link } from 'react-router-dom'
 import PeriodDetail from '../components/PeriodDetail'
+import axios from 'axios'
+
+
 
 function InformationPage() {
+  const [weathers, setWeathers] = useState({});
+
+  useEffect(()=>{
+    axios.get('https://api.weather.gov/gridpoints/TOP/31,80/forecast')
+    .then((resp)=>{
+      setWeathers(resp.data.properties.periods);
+    })
+  }, [])
+
   return (
     <div className="
       h-screen relative grid grid-rows-10 lg:grid-rows-12
@@ -25,20 +37,27 @@ function InformationPage() {
         </div>
         <div className="
           h-auto row-span-5
-          grid auto-rows-[25%] gap-3
+          grid auto-rows-[25%] gap-3 grid-cols-2
           lg:hidden overflow-scroll
           mx-3
         ">
-          <DaySimple />
-          <DaySimple />
-          <DaySimple />
-          <DaySimple />
-          <DaySimple />
-          <DaySimple />
-          <DaySimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
+          <PeriodSimple />
         </div>
         <div className="
-          opacity-70 hidden lg:grid row-span-11 auto-cols-[30%]
+          hidden lg:grid row-span-11 auto-cols-[30%]
           grid-flow-col overflow-scroll gap-3
         ">
           <div className="
